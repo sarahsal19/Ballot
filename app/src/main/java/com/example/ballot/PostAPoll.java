2c80eaecda16787b3432912d39c636753c2082cc
Modhi1 <modhi2578@gmail.com>
2022-05-08 01:40:08 +0300

    added post a poll

---
+++ app/src/main/java/com/example/ballot/PostAPoll.java
@@ -0,0 +1,16 @@
+package com.example.ballot;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PostAPoll extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_poll);
+
+    }
+
+}
