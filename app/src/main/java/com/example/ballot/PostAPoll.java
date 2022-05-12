e70d6bac27ee1ab225278c8543f5f157f95d09c0
Modhi1 <modhi2578@gmail.com>
2022-05-12 21:31:02 +0300

    added backend of post a poll

--- app/src/main/java/com/example/ballot/PostAPoll.java
+++ app/src/main/java/com/example/ballot/PostAPoll.java
@@ -1,16 +1,108 @@
 package com.example.ballot;
 
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import com.example.ballot.Sql.DBHelper;
 
-public class PostAPoll extends AppCompatActivity {
-
+public class PostAPoll extends AppCompatActivity implements LocationListener{
+    EditText title , question;
+    Button submitBtn;
+    DBHelper dbHelper;
+    protected LocationManager locationManager;
+    protected LocationListener locationListener;
+    protected Context context;
+   // String provider;
+    String latitude,longitude;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_poll);
 
+        title=findViewById(R.id.poll_title);
+        question=findViewById(R.id.poll_question);
+        submitBtn = findViewById(R.id.poll_submit);
+        // make sure of the helper
+        dbHelper = new DBHelper(this);
+        // reference so that I can get the location
+         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        // for validation:
+        // check if the 2 fields are empty
+        // if not: save the title, question and the user location to the Polls table in DB and show toast
+        // then set title and question to ""
+
+
+        // Add back button and toolbar
+
+        // onclick submit a poll button
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String titleCheck = title.getText().toString();
+                String questionCheck = question.getText().toString();
+
+                // check fields
+                if (isEmpty(title)){
+                    title.setError("Title can not be empty");
+                }
+
+                if (isEmpty(question)){
+                    question.setError("Question can not be empty");
+                }
+
+                // get user current location (method onLocationChanged)
+
+                // Add the new poll to polls table (trim them)
+                // id (PK) | title | question | latitude | longitude
+                System.out.println("lat is "+ latitude+ " long is "+longitude);
+                boolean result =dbHelper.insertPoll(titleCheck,questionCheck,latitude,longitude);
+                if (result){
+                    showAToast("Poll was submitted successfully");
+
+                    // go to home screen
+                    Intent intent = new Intent(PostAPoll.this,Home.class);
+                    title.setText("");
+                    question.setText("");
+                    startActivity(intent);
+                }else {
+                    showAToast("Something went wrong, poll was not submitted ");
+                }
+
+                dbHelper.close();
+            }
+        });
+
+    }
+
+    // set lat and long for location
+    @Override
+    public void onLocationChanged(Location location) {
+        latitude = String.valueOf(location.getLatitude());
+        longitude = String.valueOf(location.getLongitude());
+    }
+    // show a toast
+    public void showAToast(String text){
+        Toast.makeText(PostAPoll.this,text,Toast.LENGTH_LONG).show();
+    }
+
+
+    // check if text field is empty
+    private boolean isEmpty(EditText etText) {
+        if (etText.getText().toString().trim().length() > 0)
+            return false;
+
+        return true;
     }
 
 }
--- app/src/main/java/com/example/ballot/Sql/DBHelper.java
+++ app/src/main/java/com/example/ballot/Sql/DBHelper.java
@@ -6,16 +6,23 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 public class DBHelper extends SQLiteOpenHelper {
+    SQLiteDatabase DB = this.getWritableDatabase();
+
     public DBHelper(Context context ) {
         super(context,"UserData",null, 1);
     }
     @Override
     public void onCreate(SQLiteDatabase DB) {
         DB.execSQL("create Table UserDetails(userID TEXT primary key,name TEXT,password PASSWORD)");
+
+        // create polls table
+        DB.execSQL("create Table Polls(pollID INTEGER primary key AUTOINCREMENT, title TEXT,question TEXT, latitude TEXT, longitude TEXT)");
     }
     @Override
     public void onUpgrade(SQLiteDatabase DB, int i, int i1) {
         DB.execSQL("drop Table if exists UserDetails");
+
+        DB.execSQL("drop Table if exists Polls");
     }
     public Boolean insetUserData(String name,String email,String password){
         SQLiteDatabase DB = this.getWritableDatabase();
@@ -35,6 +42,25 @@ public class DBHelper extends SQLiteOpenHelper {
             return true;
         }
     }
+
+    public Boolean insertPoll(String title,String question,String latitude, String longitude){
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("title",title);
+        contentValues.put("question",question);
+        contentValues.put("latitude",latitude);
+        contentValues.put("longitude",longitude);
+
+        long result= DB.insert("Polls",null,contentValues);
+        if (result == -1){
+            return false;
+        }else {
+            DB.close();
+            return true;
+        }
+    }
+
+
     public Cursor getData(){
         SQLiteDatabase DB = this.getWritableDatabase();
         Cursor cursor = DB.rawQuery("Select * from Userdetails ",null);
